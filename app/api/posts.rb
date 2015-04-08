ea2d6995f519a44b922caa7fc394051c69a1e609
Maksimov Sergey <sergey_maksimov@etb-lab.com>
2015-04-08 13:04:02 +0300

    refactoring a little

--- app/api/posts.rb
+++ app/api/posts.rb
@@ -1,52 +1,50 @@
-module Posts
-  class API < Grape::API
-    prefix :api
-    format :json
-    formatter :json, Grape::Formatter::Jbuilder
-    version 'v1', using: :header, vendor: 'maksimov'
+class Posts < Grape::API
+  prefix :api
+  format :json
+  formatter :json, Grape::Formatter::Jbuilder
+  version 'v1', using: :header, vendor: 'maksimov'
 
-    resources :posts do
-      desc "index posts."
-      get jbuilder: 'posts/index' do
-        @posts = Post.all
-      end
+  resources :posts do
+    desc "index posts."
+    get jbuilder: 'posts/index' do
+      @posts = Post.all
+    end
 
-      desc "show post."
-      params do
-        requires :id, type: Integer, desc: "Post's ID."
-      end
-      get ':id', jbuilder: 'posts/show' do
-        @post = Post.find(params[:id])
-      end
+    desc "show post."
+    params do
+      requires :id, type: Integer, desc: "Post's ID."
+    end
+    get ':id', jbuilder: 'posts/show' do
+      @post = Post.find(params[:id])
+    end
 
-      desc "create post."
-      params do
-        requires :title, type: String, desc: "Post's title."
-        requires :body, type: String, desc: "Post's body."
-      end
-      post jbuilder: 'posts/show' do
-        @post = Post.create(declared(params))
-      end
+    desc "create post."
+    params do
+      requires :title, type: String, desc: "Post's title."
+      requires :body, type: String, desc: "Post's body."
+    end
+    post jbuilder: 'posts/show' do
+      @post = Post.create(declared(params))
+    end
 
-      desc "update post."
-      params do
-        requires :id, type: Integer, desc: "Post's ID."
-        optional :title, type: String, desc: "Post's title."
-        optional :body, type: String, desc: "Post's body."
-      end
-      put ':id', jbuilder: 'posts/show' do
-        @post = Post.find(params[:id])
-        @post.update_attributes(params)
-      end
+    desc "update post."
+    params do
+      requires :id, type: Integer, desc: "Post's ID."
+      optional :title, type: String, desc: "Post's title."
+      optional :body, type: String, desc: "Post's body."
+    end
+    put ':id', jbuilder: 'posts/show' do
+      @post = Post.find(params[:id])
+      @post.update_attributes(params)
+    end
 
-      desc "destroy post."
-      params do
-        requires :id, type: Integer, desc: "Post's ID."
-      end
-      delete ':id' do
-        post = Post.find(params[:id]).destroy!
-        body status: 'ok'
-      end
+    desc "destroy post."
+    params do
+      requires :id, type: Integer, desc: "Post's ID."
+    end
+    delete ':id' do
+      post = Post.find(params[:id]).destroy!
+      body status: 'ok'
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount Posts::API => '/'
+  mount Posts => '/'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
