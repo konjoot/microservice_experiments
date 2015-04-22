2313627ae48de1fb627ddd9762ecb35e462fdd3a
Maksimov Sergey <sergey_maksimov@etb-lab.com>
2015-04-22 14:48:33 +0300

    working on gin-gonic app
    * mediators refactoring

--- gin-gonic/src/app/controllers/posts.go
+++ gin-gonic/src/app/controllers/posts.go
@@ -2,7 +2,7 @@ package controllers
 
 import (
   "github.com/gin-gonic/gin"
-  . "app/mediators/post"
+  . "app/mediators"
 )
 
 
--- gin-gonic/src/app/mediators/post.go
+++ gin-gonic/src/app/mediators/post.go
@@ -1,4 +1,4 @@
-package post
+package mediators
 
 import (
     "app/models"
